00ed3fc74ef2691d2061ce28be9e16eca41e4c6d
connormullett <connormullett@gmail.com>
2021-09-30 15:59:27 -0400

    scaffold power migrator

--- vm/state_migration/src/nv10/mod.rs
+++ vm/state_migration/src/nv10/mod.rs
@@ -3,9 +3,11 @@ mod market;
 mod miner;
 mod multisig;
 mod paych;
+mod power;
 mod utils;
 
 pub use miner::miner_migrator_v3;
 pub use multisig::*;
 pub use paych::*;
+pub use power::*;
 pub use utils::*;
